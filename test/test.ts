ba7f3bb95517b602e15b5d1e02b5f1fa574b47bc
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2025-05-21 00:03:04 -0400

    List other scenarios we should support

--- test/test.ts
+++ test/test.ts
@@ -19,6 +19,90 @@ describe("Zod v3 to v4", () => {
       await runScenario("error-customization.drop-error-map");
     });
   });
+
+  // https://zod.dev/v4/changelog?id=zoderror
+  describe("ZodError", () => {
+    it.todo("replaces deprecated `.format()` with `z.treeifyError()`");
+
+    it.todo("replaces deprecated `.flatten()` with `z.treeifyError()`");
+
+    it.todo("drops `.formErrors()`");
+
+    it.todo(
+      "replaces deprecated `.addIssue()` and `.addIssues()` with direct pushes to `.issues`",
+    );
+  });
+
+  // https://zod.dev/v4/changelog?id=znumber
+  describe("z.number()", () => {
+    it.todo("replaces deprecated `z.number().safe()` with `z.int()`");
+
+    it.todo("replaces `z.number().int()` with `z.int()`");
+  });
+
+  // https://zod.dev/v4/changelog?id=zstring
+  describe("z.string()", () => {
+    it.todo("replaces deprecated `z.string().*()` with top-level APIs");
+
+    it.todo(
+      "replaces dropped `z.string().ip()` with `z.union([z.ipv4(), z.ipv6()])`",
+    );
+
+    it.todo(
+      "replaces dropped `z.string().cidr()` with `z.union([z.cidrv4(), z.cidrv6()])`",
+    );
+  });
+
+  // https://zod.dev/v4/changelog?id=default-updates
+  describe("z.default()", () => {
+    it.todo("replaces `z.default()` with `z.prefault()` to preserve behavior");
+  });
+
+  // https://zod.dev/v4/changelog?id=zobject
+  describe("z.object()", () => {
+    it.todo(
+      "replaces deprecated `z.object().strict()` with `z.strictObject()`",
+    );
+
+    it.todo(
+      "replaces deprecated `z.object().passthrough()` with `z.looseObject()`",
+    );
+
+    it.todo("replaces deprecated `z.strip()` with `z.object()`");
+
+    it.todo("replaces dropped `z.nonstrict()` with `z.object()`");
+  });
+
+  // https://zod.dev/v4/changelog?id=znativeenum-deprecated
+  describe("z.nativeEnum()", () => {
+    it.todo("replaces deprecated `z.nativeEnum()` with `z.enum()`");
+  });
+
+  // https://zod.dev/v4/changelog?id=zarray
+  describe("z.array()", () => {
+    it.todo("replaces `z.nonEmpty()` with `z.tuple()` to preserve behavior");
+  });
+
+  // https://zod.dev/v4/changelog?id=zpromise-deprecated
+  describe("z.promise()", () => {
+    it.todo("replaces `z.promise()` with an await call before parsing");
+
+    it.todo("replaces `z.function().promise()` with `z.implementAsync()`");
+  });
+
+  // https://zod.dev/v4/changelog?id=zfunction
+  describe("z.function()", () => {
+    it.todo(
+      "replaces `z.args()` and `z.returns()` with `z.function()` parameters",
+    );
+  });
+
+  // https://zod.dev/v4/changelog?id=zrecord
+  describe("z.record()", () => {
+    it.todo(
+      "replaces dropped single-argument `z.record()` with a double-arguments version",
+    );
+  });
 });
 
 async function runScenario(fixturePath: string) {
