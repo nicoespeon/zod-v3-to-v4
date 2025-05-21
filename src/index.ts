13df5ecea5e35a99271a520c6fb06104c4e99df1
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2025-05-20 23:15:16 -0400

    Convert imports to "zod/v4"

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,48 @@
-import { type SourceFile, SyntaxKind } from "ts-morph";
+import {
+  ExpressionStatement,
+  ImportDeclaration,
+  type SourceFile,
+  SyntaxKind,
+  VariableDeclaration,
+} from "ts-morph";
 
 export function handleSourceFile(sourceFile: SourceFile): string | undefined {
-  collectZodReferences(sourceFile).map((node) => {
+  const importDeclarations = collectZodImportDeclarations(sourceFile);
+
+  importDeclarations.forEach((importDeclaration) => {
+    importDeclaration.setModuleSpecifier("zod/v4");
+  });
+
+  collectZodReferences(importDeclarations).map((node) => {
     const parentStatement =
       node.getFirstAncestorByKind(SyntaxKind.ExpressionStatement) ??
       node.getFirstAncestorByKind(SyntaxKind.VariableDeclaration);
 
-    parentStatement
-      ?.getDescendantsOfKind(SyntaxKind.Identifier)
-      .filter(
-        (id) =>
-          id.getParentIfKind(SyntaxKind.PropertyAssignment) &&
-          id.getParent()?.getParentIfKind(SyntaxKind.ObjectLiteralExpression) &&
-          id.getText() === "message",
-      )
-      .forEach((id) => {
-        id.replaceWithText("error");
-      });
+    convertMessageKeyToError(parentStatement);
   });
 
   return sourceFile.getFullText();
 }
 
-function collectZodReferences(sourceFile: SourceFile) {
-  return sourceFile.getImportDeclarations().flatMap((importDeclaration) => {
-    const isZodImport = ["zod", "zod/v4"].includes(
-      importDeclaration.getModuleSpecifierValue(),
-    );
-    if (!isZodImport) {
-      return [];
-    }
+function convertMessageKeyToError(
+  node: ExpressionStatement | VariableDeclaration | undefined,
+) {
+  node
+    ?.getDescendantsOfKind(SyntaxKind.Identifier)
+    .filter(
+      (id) =>
+        id.getParentIfKind(SyntaxKind.PropertyAssignment) &&
+        id.getParent()?.getParentIfKind(SyntaxKind.ObjectLiteralExpression) &&
+        id.getText() === "message",
+    )
+    .forEach((id) => {
+      id.replaceWithText("error");
+    });
+}
 
-    return importDeclaration.getNamedImports().flatMap((namedImport) => {
+function collectZodReferences(importDeclarations: ImportDeclaration[]) {
+  return importDeclarations.flatMap((importDeclaration) =>
+    importDeclaration.getNamedImports().flatMap((namedImport) => {
       const namedNode = namedImport.getNameNode();
       if (!namedNode.isKind(SyntaxKind.Identifier)) {
         return [];
@@ -40,6 +51,14 @@ function collectZodReferences(sourceFile: SourceFile) {
       return namedNode
         .findReferencesAsNodes()
         .filter((node) => node !== namedNode);
-    });
-  });
+    }),
+  );
+}
+
+function collectZodImportDeclarations(sourceFile: SourceFile) {
+  return sourceFile
+    .getImportDeclarations()
+    .filter((importDeclaration) =>
+      ["zod", "zod/v4"].includes(importDeclaration.getModuleSpecifierValue()),
+    );
 }
