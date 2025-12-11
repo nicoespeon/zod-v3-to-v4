6845fc1f8e435d97b149ce0a2accc4e74619d188
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2025-12-11 16:15:38 -0500

    Add test case for SafeParseReturnType

--- test/__scenarios__/zod-types.deleted.input.ts
+++ test/__scenarios__/zod-types.deleted.input.ts
@@ -1,5 +1,12 @@
-import { AnyZodObject, z } from "zod";
+import { AnyZodObject, SafeParseReturnType, z } from "zod";
 
 export function promptCallback(input: z.infer<AnyZodObject>) {
   return `${input.greeting ?? "Hello"} ${input.user.firstName}! You are from ${input.user.location.city}, ${input.user.location.country}.`;
 }
+
+export function safeParseResult<
+  SameIAndO extends SafeParseReturnType<string, string>,
+  // This will only keep the output type
+  // See https://github.com/colinhacks/zod/issues/5195
+  DifferentIAndO extends SafeParseReturnType<number, string>,
+>(_: SameIAndO, __: DifferentIAndO) {}
--- test/__scenarios__/zod-types.deleted.output.ts
+++ test/__scenarios__/zod-types.deleted.output.ts
@@ -1,5 +1,12 @@
-import { z, ZodRecord } from "zod/v4";
+import { z, ZodRecord, ZodSafeParseResult } from "zod/v4";
 
 export function promptCallback(input: z.infer<ZodRecord<any, any>>) {
   return `${input.greeting ?? "Hello"} ${input.user.firstName}! You are from ${input.user.location.city}, ${input.user.location.country}.`;
 }
+
+export function safeParseResult<
+  SameIAndO extends ZodSafeParseResult<string>,
+  // This will only keep the output type
+  // See https://github.com/colinhacks/zod/issues/5195
+  DifferentIAndO extends ZodSafeParseResult<string>,
+>(_: SameIAndO, __: DifferentIAndO) {}
